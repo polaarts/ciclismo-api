5abaad32ab251fda2e4a56c7c0a3914d9a3c2067
Samuel Angulo <samuel.angulo_c@mail.udp.cl>
2024-01-08 21:13:53 -0300

    fix: add cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express, { type Express } from 'express'
 import productApp from './product/product.app'
 import { PORT } from './config'
+import cors from 'cors'
 
 const app: Express = express()
+
+app.use(cors())
 
 app.use('/products', productApp)
 
